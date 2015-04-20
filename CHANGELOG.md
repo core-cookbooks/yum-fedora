f4d7fe0a24f72e9def9de980b8c597f0b38764fa
core-cookbooks <nagalakshmi.n@cloudenablers.com>
2015-04-20 19:12:58 +0530

    Initial Release

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+yum-fedora Cookbook CHANGELOG
+======================
+This file is used to list changes made in each version of the yum-fedora cookbook.
+
+v0.3.0
+------
+initial release
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name 'yum-fedora'
+maintainer 'Cloudenablers'
+maintainer_email 'nagalakshmi.n@cloudenablers.com'
+license 'Apache 2.0'
+description 'Installs/Configures yum-fedora'
+version '0.3.0'
+
+depends 'yum', '~> 3.0'
